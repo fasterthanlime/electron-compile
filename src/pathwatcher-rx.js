9863ea3ceff680e2877d4cd45520e8b17caa2135
Amos Wenger <amos@itch.io>
2017-08-24 19:49:55 +0200

    Work around https://github.com/electron/electron-compile/issues/227

--- src/pathwatcher-rx.js
+++ src/pathwatcher-rx.js
@@ -7,19 +7,29 @@ import 'rxjs/add/operator/publish';
 
 export function watchPathDirect(directory) {
   return Observable.create((subj) => {
+    let watcher = null;
     let dead = false;
 
-    const watcher = fs.watch(directory, {}, (eventType, fileName) => {
-      if (dead) return;
-      subj.next({eventType, fileName});
-    });
+    try {
+      watcher = fs.watch(directory, {}, (eventType, fileName) => {
+        if (dead) return;
+        subj.next({eventType, fileName});
+      });
 
-    watcher.on('error', (e) => {
+      watcher.on('error', (e) => {
+        dead = true;
+        subj.error(e);
+      });
+    } catch (e) {
       dead = true;
-      subj.error(e);
-    });
-
-    return new Subscription(() => { if (!dead) { watcher.close(); } });
+      if (e.code === "ENOENT") {
+        // that's ok, we just won't watch the non-existent directory
+      } else {
+        // if it's not that, let's log and continue
+        console.warn(e.message);
+      }
+    }
+    return new Subscription(() => { if (!dead && watcher) { watcher.close(); } });
   });
 }
 
